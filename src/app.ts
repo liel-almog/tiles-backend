a6d392b876b2929cd218fa86a962bd740f9dfe58
Liel Almog <liel.almog@agamimp.net>
2021-12-28 10:24:42 +0200

    Add boilerplate app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,35 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import { connectToDatabase } from "./utils/database";
+import { usersRouter } from "./routes/route.users";
+import { tilesRouter } from "./routes/route.tiles";
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+connectToDatabase();
+
+app.use(cors());
+
+app.use("/user", usersRouter);
+app.use("/tile", tilesRouter);
+
+app.get("/", (_req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/signup", (_req, res) => {
+  res.send({ hello: "world" });
+});
+app.post("/login", (_req, res) => {
+  res.send({ login: "liel" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(port, () => {
+  // tslint:disable-next-line: no-console
   console.log("Welcome to node with type script");
 });
