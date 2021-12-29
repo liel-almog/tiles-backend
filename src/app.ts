103a53a4e76090d5d142a207faa434da14fb80e3
Liel Almog <liel.almog@agamimp.net>
2021-12-29 10:35:55 +0200

    Feat: Merge all Routes to app

--- src/app.ts
+++ src/app.ts
@@ -3,33 +3,37 @@ import cors from "cors";
 import { connectToDatabase } from "./utils/database";
 import { usersRouter } from "./routes/route.users";
 import { tilesRouter } from "./routes/route.tiles";
+import { authRouter } from "./routes/route.auth";
+import 'reflect-metadata';
 
 const app = express();
 const port = 8080;
 
-connectToDatabase();
-
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/user", usersRouter);
 app.use("/tile", tilesRouter);
+app.use("/auth", authRouter)
 
 app.get("/", (_req, res) => {
-  res.send("Hello World");
-});
-
-app.post("/signup", (_req, res) => {
-  res.send({ hello: "world" });
-});
-app.post("/login", (_req, res) => {
-  res.send({ login: "liel" });
+  res.send("Hello World😜");
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(port, () => {
-  // tslint:disable-next-line: no-console
-  console.log("Welcome to node with type script");
-});
+connectToDatabase()
+  .then(() => {
+    app.listen(port, () => {
+      // tslint:disable-next-line: no-console
+      console.log("Welcome to node with type script");
+    });
+  })
+  .catch((error: Error) => {
+    // tslint:disable-next-line: no-console
+    console.error("Database connection failed", error);
+    process.exit();
+  });
