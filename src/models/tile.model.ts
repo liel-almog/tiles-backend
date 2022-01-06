d2e5411d220b6f218508c6a769806b8f175d8a46
Liel Almog <liel.almog@agamimp.net>
2022-01-06 15:36:01 +0200

    Feat: Add JWT auth middleware

--- src/models/tile.model.ts
+++ src/models/tile.model.ts
@@ -3,12 +3,12 @@ import { ObjectId } from "mongodb";
 import { ExposeId } from "../utils/ExposeId";
 
 export default class Tile {
-  // @ExposeId()
-  @Exclude()
+  @ExposeId()
+  // @Exclude()
   public _id?: ObjectId;
   public color: string;
-  public createdAt = new Date();
-  public updatedAt = new Date();
+  // public createdAt = new Date();
+  // public updatedAt = new Date();
 
   constructor(color: string) {
     this.color = color;
