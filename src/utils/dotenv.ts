57205199b124d16a35093ad5bf7eb12b2d73f94b
Liel Almog <liel.almog@agamimp.net>
2022-01-06 15:37:43 +0200

    Feat: Add dotenv and auth middleware

--- src/utils/dotenv.ts
+++ src/utils/dotenv.ts
@@ -6,4 +6,5 @@ export default {
   DB_CONN_STRING: process.env.DB_CONN_STRING ?? "",
   USERS_COLLECTION_NAME: process.env.USERS_COLLECTION_NAME ?? "",
   TILES_COLLECTION_NAME: process.env.TILES_COLLECTION_NAME ?? "",
+  JWT_SECRET: process.env.JWT_SECRET as string
 };
