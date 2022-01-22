fdc9a10332888e419f5a0cd5585ecfb8ce1af398
Liel Almog <liel.almogg@gmail.com>
2022-01-22 15:24:53 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: process.env.FRONTEND_URL });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT);
 }
