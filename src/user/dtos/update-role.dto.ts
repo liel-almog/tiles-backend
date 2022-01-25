d082257de4b784cd8f6707914f13350aa68e22b4
Liel Almog <70017134+liel-almog@users.noreply.github.com>
2022-01-25 15:13:40 +0200

    Add update roles dto

--- src/user/dtos/update-role.dto.ts
+++ src/user/dtos/update-role.dto.ts
@@ -1,3 +1,19 @@
-class UpdateRole {
-  
+import { Role } from "@prisma/client";
+import { ObjectId } from "bson";
+import { Type } from "class-transformer";
+import { IsArray, IsEnum, IsMongoId, ValidateNested } from "class-validator";
+
+class UpdateRoles {
+  @IsMongoId()
+  _id: ObjectId;
+
+  @IsEnum(Role)
+  role: Role;
+}
+
+export class UpdateRolesDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateRoles)
+  updateRoles: UpdateRoles[];
 }
