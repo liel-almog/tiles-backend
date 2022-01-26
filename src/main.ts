2886abecd17a40dffcf03e50c64c2d8d4190a466
Liel Almog <70017134+liel-almog@users.noreply.github.com>
2022-01-26 15:58:01 +0200

    Add ms, @nestjs/jwt, cookieparser
    Add ms, @nestjs/jwt, cookieparser

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { AppModule } from "./app.module";
 
@@ -8,6 +9,7 @@ async function bootstrap() {
   app.enableCors({ credentials: true, origin: process.env.FRONTEND_URL });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(helmet());
+  app.use(cookieParser())
   await app.listen(process.env.PORT);
 }
 bootstrap();
