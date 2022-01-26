ab81342ebe15011a08b23b4e8f882abd87a78532
Liel Almog <70017134+liel-almog@users.noreply.github.com>
2022-01-26 09:43:00 +0200

    Add and use helmet Install passport and @nestjs/passport

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import helmet from 'helmet';
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ credentials: true, origin: process.env.FRONTEND_URL });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(helmet());
   await app.listen(process.env.PORT);
 }
 bootstrap();
